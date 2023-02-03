aef427733cc880c687aeaccfa3720e4b5034fc1b
JulieBodette <jr3python@gmail.com>
2023-02-03 13:25:34 -0500

    Create MenuItem.cs

---
+++ RecipeDatabase/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipeDatabase
+{
+    public class MenuItem
+    {
+        public string FoodName { get; set; }
+        public string FoodGroup { get; set; }
+        public string Link { get; set; }
+    }
+}
