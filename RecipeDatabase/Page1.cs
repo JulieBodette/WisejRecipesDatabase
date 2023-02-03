b87705db58ce638032f0787b77c877976484f375
JulieBodette <jr3python@gmail.com>
2023-02-03 13:10:27 -0500

    initial commit

---
+++ RecipeDatabase/Page1.cs
@@ -0,0 +1,12 @@
+﻿using Wisej.Web;
+
+namespace RecipeDatabase
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
