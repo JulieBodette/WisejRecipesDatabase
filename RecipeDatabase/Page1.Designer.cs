fbfe21c88090da82dddfd84a94e54b127280090a
JulieBodette <jr3python@gmail.com>
2023-02-06 14:01:33 -0500

    Added labels and additional datagridview

--- RecipeDatabase/Page1.Designer.cs
+++ RecipeDatabase/Page1.Designer.cs
@@ -29,32 +29,69 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new Wisej.Web.DataGridView();
+            this.label1 = new Wisej.Web.Label();
+            this.label2 = new Wisej.Web.Label();
+            this.dataGridView2 = new Wisej.Web.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
-            this.dataGridView1.Location = new System.Drawing.Point(216, 83);
+            this.dataGridView1.Location = new System.Drawing.Point(24, 70);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(799, 296);
+            this.dataGridView1.Size = new System.Drawing.Size(569, 309);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "dbo.Menu_Items";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(982, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "dbo.Food_Group";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.Location = new System.Drawing.Point(736, 70);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(501, 308);
+            this.dataGridView2.TabIndex = 4;
             // 
             // Page1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
             this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Page1";
-            this.Size = new System.Drawing.Size(1131, 401);
+            this.Size = new System.Drawing.Size(1133, 384);
             this.Load += new System.EventHandler(this.Page1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Wisej.Web.DataGridView dataGridView1;
+        private Wisej.Web.Label label1;
+        private Wisej.Web.Label label2;
+        private Wisej.Web.DataGridView dataGridView2;
     }
 }
 
